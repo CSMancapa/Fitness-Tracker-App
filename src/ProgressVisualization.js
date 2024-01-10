60493674a9c373f19bc704bb3e8e7ded017746ba
CSMancapa <cmancapa@gmail.com>
2024-01-10 14:22:04 +0200

    Charts were not rendering so had to debug

--- src/ProgressVisualization.js
+++ src/ProgressVisualization.js
@@ -1,59 +1,50 @@
-// ProgressVisualization.js
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import './ProgressVisualization.css';
 
 const ProgressVisualization = ({ progressData }) => {
   console.log(progressData);
+
   return (
     <div>
       {/* Duration Chart */}
-      <div>
-        <LineChart width={800} height={400} data={progressData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="Duration (min)" stroke="#8884d8" />
-        </LineChart>
-      </div>
+      <LineChart width={800} height={400} data={progressData}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="totalDuration" stroke="#8884d8" />
+      </LineChart>
 
       {/* Distance Chart */}
-      <div>
-        <LineChart width={800} height={400} data={progressData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="Distance (km)" stroke="#82ca9d" />
-        </LineChart>
-      </div>
+      <LineChart width={800} height={400} data={progressData}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="totalDistance" stroke="#82ca9d" />
+      </LineChart>
 
       {/* Heart Rate Chart */}
-      <div>
-        <LineChart width={800} height={400} data={progressData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="Avg Heart Rate (bpm)" stroke="#8884d8" />
-        </LineChart>
-      </div>
+      <LineChart width={800} height={400} data={progressData}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="averageHeartRate" stroke="#8884d8" />
+      </LineChart>
 
       {/* Calories Chart */}
-      <div>
-        <LineChart width={800} height={400} data={progressData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="Calories (cal)" stroke="#ff7300" />
-        </LineChart>
-      </div>
+      <LineChart width={800} height={400} data={progressData}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="totalCalories" stroke="#ff7300" />
+      </LineChart>
     </div>
   );
 };
